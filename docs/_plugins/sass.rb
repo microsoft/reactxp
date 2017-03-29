79ba23480a82e6069f216f774654ac116096585b
ericptraut <erictr@microsoft.com>
2017-03-28 19:23:36 -0700

    Added SASS converter.

---
+++ docs/_plugins/sass.rb
@@ -0,0 +1,22 @@
+module Jekyll
+  require 'sass'
+  class SassConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /scss/i
+    end
+
+    def output_ext(ext)
+      ".css"
+    end
+
+    def convert(content)
+      engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./css/"])
+      engine.render
+    rescue StandardError => e
+      puts "!!! SASS Error: " + e.message
+    end
+  end
+end
