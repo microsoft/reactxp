b634d792213733dbeaec024fe61308d401e53d2a
Mohsen Azimi <mazimi@lyft.com>
2017-04-12 08:55:21 -0700

    add types and fix config

--- samples/hello-world/webpack.config.ts
+++ samples/hello-world/webpack.config.ts
@@ -1,6 +1,6 @@
 import * as webpack from 'webpack';
 
-const config: webpack.Configuration {
+const config: webpack.Configuration = {
     entry: "./src/index.tsx",
     output: {
         filename: "bundle.js",
@@ -18,7 +18,7 @@ const config: webpack.Configuration {
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
         ]
     },
 };
