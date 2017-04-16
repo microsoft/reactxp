1ab616229ba6bcd3eac2ec445c3d798d419d7054
Eric Traut <erictr@microsoft.com>
2017-04-16 10:41:16 -0700

    Added sample code for a simple animated ToggleSwitch in the Hello World app.

--- samples/hello-world/src/App.tsx
+++ samples/hello-world/src/App.tsx
@@ -4,6 +4,12 @@
 
 import RX = require('reactxp');
 
+import ToggleSwitch from './ToggleSwitch';
+
+interface AppState {
+    toggleValue?: boolean;
+}
+
 const styles = {
     container: RX.Styles.createViewStyle({
         flex: 1,
@@ -27,11 +33,16 @@ const styles = {
     }),
     docLink: RX.Styles.createLinkStyle({
         fontSize: 16,
-        color: 'blue'
+        color: 'blue',
+        marginBottom: 40
+    }),
+    toggleTitle: RX.Styles.createTextStyle({
+        fontSize: 16,
+        color: 'black'
     })
 };
 
-class App extends RX.Component<null, null> {
+class App extends RX.Component<null, AppState> {
     private _translationValue: RX.Animated.Value;
     private _animatedStyle: RX.Types.AnimatedTextStyleRuleSet;
 
@@ -46,6 +57,10 @@ class App extends RX.Component<null, null> {
                 }
             ]
         });
+
+        this.state = {
+            toggleValue: true
+        };
     }
 
     componentDidMount() {
@@ -74,8 +89,25 @@ class App extends RX.Component<null, null> {
                 <RX.Link style={ styles.docLink } url={ 'https://microsoft.github.io/reactxp/docs' }>
                     View ReactXP documentation
                 </RX.Link>
+
+                <RX.Text style={ styles.toggleTitle }>
+                    Here is a simple control built using ReactXP
+                </RX.Text>
+                <ToggleSwitch
+                    value={ this.state.toggleValue }
+                    onChange={ this._onChangeToggle }
+                />
             </RX.View>
         );
+    }
+
+    // Note that we define this as a variable rather than a normal method. Using this
+    // method, we prebind the method to this component instance. This prebinding ensures
+    // that each time we pass the variable as a prop in the render function, it will
+    // not change. We want to avoid unnecessary prop changes because this will trigger
+    // extra work within React's virtual DOM diffing mechanism.
+    private _onChangeToggle = (newValue: boolean) => {
+        this.setState({ toggleValue: newValue });
     }
 }
 
