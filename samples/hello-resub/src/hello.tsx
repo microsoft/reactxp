f721554d6784f6fe0c3f3371baf4649326f55aac
bidanjun <15345479@qq.com>
2017-04-20 11:53:18 +0800

    add a button to show/hide hello resub string.

--- samples/hello-resub/src/hello.tsx
+++ samples/hello-resub/src/hello.tsx
@@ -6,20 +6,29 @@ import HelloStore =require('./helloStore');
 
 interface HelloState {
     Hello?: string;
+    showTitle: boolean;
 }
 
 class Hello extends ComponentBase<{}, HelloState> {
     protected _buildState(props: {}, initialBuild: boolean): HelloState {
         return {
-            Hello: HelloStore.getHello()
+            Hello: HelloStore.getHello(),
+            showTitle: HelloStore.getShowTitle(),
         }
     }
 
+    //add onclick to show/hide hello resub.
     render(): JSX.Element | null  {
         return (
-            <RX.Text>
-                { this.state.Hello }
-            </RX.Text>
+            <RX.View>
+                <RX.Button title="click here for show/hide text" 
+                    onPress={() => HelloStore.switchShowTitle()}>
+                    {"click here to show/hide"}
+                </RX.Button>
+                < RX.Text>
+                    { this.state.showTitle? this.state.Hello :null }
+                </RX.Text>            
+            </RX.View>
         );
     }
 }
--- samples/hello-resub/src/helloStore.ts
+++ samples/hello-resub/src/helloStore.ts
@@ -6,12 +6,23 @@ class HelloStore extends StoreBase {
 
     //_helloString:string ,should give a type here
     private _helloString: string = 'hello resub!';
+    private _isShowTitle: boolean = true;
+
+     switchShowTitle() {
+        this._isShowTitle = !this._isShowTitle;
+        this.trigger();
+    }
 
     @autoSubscribe
     // this method should return a string type
     getHello():string {
         return this._helloString;
     }
+    @autoSubscribe
+    // this method should return a string type
+    getShowTitle():boolean {
+        return this._isShowTitle;
+    }    
 }
 
 // export a instance of class HelloStore
