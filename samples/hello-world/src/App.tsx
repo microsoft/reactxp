0fca665c2141efaf2d18529c873ecf80cd264a27
Eric Traut <erictr@microsoft.com>
2017-05-16 12:01:35 -0700

    Added Navigator example to hello-world sample.

--- samples/hello-world/src/App.tsx
+++ samples/hello-world/src/App.tsx
@@ -3,183 +3,69 @@
 */
 
 import RX = require('reactxp');
-import { default as RXVideo } from 'reactxp-video';
 
-import ProgressIndicator from './ProgressIndicator';
-import ToggleSwitch from './ToggleSwitch';
+import MainPanel = require('./MainPanel');
+import SecondPanel = require('./SecondPanel');
 
-interface AppState {
-    toggleValue?: boolean;
-    progressValue?: number;
+enum NavigationRouteId {
+    MainPanel,
+    SecondPanel
 }
 
 const styles = {
-    scroll: RX.Styles.createScrollViewStyle({
-        alignSelf: 'stretch',
+    navCardStyle: RX.Styles.createViewStyle({
         backgroundColor: '#f5fcff'
-    }),
-    container: RX.Styles.createViewStyle({
-        padding: 16,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }),
-    helloWorld: RX.Styles.createTextStyle({
-        fontSize: 48,
-        fontWeight: 'bold',
-        marginBottom: 28
-    }),
-    welcome: RX.Styles.createTextStyle({
-        fontSize: 32,
-        marginBottom: 12
-    }),
-    instructions: RX.Styles.createTextStyle({
-        fontSize: 16,
-        color: '#aaa',
-        marginBottom: 16
-    }),
-    docLink: RX.Styles.createLinkStyle({
-        fontSize: 16,
-        color: 'blue',
-        marginBottom: 16
-    }),
-    titleText: RX.Styles.createTextStyle({
-        fontSize: 16,
-        textAlign: 'center',
-        marginTop: 12,
-        color: 'black'
-    }),
-    videoTitleText: RX.Styles.createTextStyle({
-        marginBottom: 8
-    }),
-    progressMargin: RX.Styles.createViewStyle({
-        margin: 8
-    }),
-    video: RX.Styles.createViewStyle({
-        height: 176,
-        width: 320
     })
 };
 
-class App extends RX.Component<null, AppState> {
-    private _translationValue: RX.Animated.Value;
-    private _animatedStyle: RX.Types.AnimatedTextStyleRuleSet;
-    private _progressTimerToken: number;
-
-    constructor() {
-        super();
-
-        this._translationValue = new RX.Animated.Value(-100);
-        this._animatedStyle = RX.Styles.createAnimatedTextStyle({
-            transform: [
-                {
-                    translateY: this._translationValue
-                }
-            ]
-        });
-
-        this.state = {
-            toggleValue: true,
-            progressValue: 0
-        };
-    }
+class App extends RX.Component<null, null> {
+    private _navigator: RX.Navigator;
 
     componentDidMount() {
-        let animation = RX.Animated.timing(this._translationValue, {
-              toValue: 0,
-              easing: RX.Animated.Easing.OutBack(),
-              duration: 500
-            }
-        );
-
-        animation.start();
-
-        this._startProgressIndicator();
+        this._navigator.immediatelyResetRouteStack([{
+            routeId: NavigationRouteId.MainPanel,
+            sceneConfigType: RX.Types.NavigatorSceneConfigType.Fade
+        }]);
     }
 
-    componentWillUnmount() {
-        this._stopProgressIndicator();
-    }
-
-    render(): JSX.Element | null {
+    render() {
         return (
-            <RX.ScrollView style={ styles.scroll }>
-                <RX.View style={ styles.container }>
-                    <RX.Animated.Text style={ [styles.helloWorld, this._animatedStyle] }>
-                        Hello World
-                    </RX.Animated.Text>
-                    <RX.Text style={ styles.welcome }>
-                        Welcome to ReactXP
-                    </RX.Text>
-                    <RX.Text style={ styles.instructions }>
-                        Edit App.tsx to get started
-                    </RX.Text>
-                    <RX.Link style={ styles.docLink } url={ 'https://microsoft.github.io/reactxp/docs' }>
-                        View ReactXP documentation
-                    </RX.Link>
-
-                    <RX.Text style={ styles.titleText }>
-                        Here is a simple control built using ReactXP
-                    </RX.Text>
-                    <ToggleSwitch
-                        value={ this.state.toggleValue }
-                        onChange={ this._onChangeToggle }
-                    />
-
-                    <RX.Text style={ styles.titleText }>
-                        Here is an SVG image using the ImageSvg extension
-                    </RX.Text>
-                    <ProgressIndicator
-                        style={ styles.progressMargin }
-                        progress={ this.state.progressValue }
-                        fillColor={ '#ddd' }
-                        size={ 32 }
-                    />
-
-                    <RX.Text style={ [styles.titleText, styles.videoTitleText] }>
-                        Here is a video using the Video extension
-                    </RX.Text>
-                    <RXVideo
-                        ref='video'
-                        style={ styles.video }
-                        source={ 'https://www.w3schools.com/html/mov_bbb.mp4' }
-                        loop={ true }
-                        onCanPlay={ this._playVideo }
-                    />
-                </RX.View>
-            </RX.ScrollView>
-
+            <RX.Navigator
+                ref={ this._onNavigatorRef }
+                renderScene={ this._renderScene }
+                cardStyle={ styles.navCardStyle }
+            />
         );
     }
 
-    private _playVideo = () => {
-        const video = this.refs['video'] as RXVideo;
-        if (video) {
-            video.mute(true);
-            video.play();
+    private _onNavigatorRef = (navigator: RX.Navigator) => {
+        this._navigator = navigator;
+    }
+
+    private _renderScene = (navigatorRoute: RX.Types.NavigatorRoute) => {
+        switch (navigatorRoute.routeId) {
+            case NavigationRouteId.MainPanel:
+                return <MainPanel onPressNavigate={ this._onPressNavigate } />
+
+            case NavigationRouteId.SecondPanel:
+                return <SecondPanel onNavigateBack={ this._onPressBack } />
         }
+
+        return null;
     }
 
-    private _startProgressIndicator() {
-        this._progressTimerToken = window.setInterval(() => {
-            const newProgressValue = (this.state.progressValue + 0.02) % 1;
-            this.setState({ progressValue: newProgressValue });
-        }, 1000 / 15);
+    private _onPressNavigate = () => {
+        this._navigator.push({
+            routeId: NavigationRouteId.SecondPanel,
+            sceneConfigType: RX.Types.NavigatorSceneConfigType.FloatFromRight,
+            customSceneConfig: {
+                hideShadow: true
+            }
+        });
     }
 
-    private _stopProgressIndicator() {
-        if (this._progressTimerToken) {
-            window.clearInterval(this._progressTimerToken);
-            this._progressTimerToken = undefined;
-        }
-    }
-
-    // Note that we define this as a variable rather than a normal method. Using this
-    // method, we prebind the method to this component instance. This prebinding ensures
-    // that each time we pass the variable as a prop in the render function, it will
-    // not change. We want to avoid unnecessary prop changes because this will trigger
-    // extra work within React's virtual DOM diffing mechanism.
-    private _onChangeToggle = (newValue: boolean) => {
-        this.setState({ toggleValue: newValue });
+    private _onPressBack = () => {
+        this._navigator.pop();
     }
 }
 
