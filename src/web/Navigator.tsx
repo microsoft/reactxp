a8355689c8698bfd51f70c6963123003f02413ed
Ryan Thomas <Ryan.Thomas@rhinobyte.com>
2017-05-18 12:19:57 -0500

    NavigatorFix - Updated src/web/Navigator.tsx constructor to initialize the presentedIndex and transitionFromIndex. Fixes NaN and an initial transition error

--- src/web/Navigator.tsx
+++ src/web/Navigator.tsx
@@ -146,12 +146,18 @@ export class Navigator extends RX.Navigator<NavigatorState> {
         if(initialRouteStack.length === 0 && initialRoute) {
             initialRouteStack.push(initialRoute);
         }
+
+        // need to initialize these or it will error 
+        (this.state as any).presentedIndex = 0;
+        (this.state as any).transitionFromIndex = null;
         
 
         // Default navigator state
         this.state = {
             sceneConfigStack: [],
+            presentedIndex: 0,
             routeStack: initialRouteStack,
+            transitionFromIndex: null,
             transitionQueue: []
         };
     }
