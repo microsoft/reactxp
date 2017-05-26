5a3e38cbb7bc6ebee396422569107823292bfb65
David de Regt <dadere@microsoft.com>
2017-05-25 17:44:09 -0700

    Adding JoFuria's TodoList sample app

---
+++ samples/TodoList/src/App.tsx
@@ -0,0 +1,92 @@
+/*
+* This file demonstrates a basic ReactXP app.
+*/
+
+import RX = require('reactxp');
+
+import MainPanel = require('./MainPanel');
+import SecondPanel = require('./SecondPanel');
+import EditTodoPanel = require('./EditTodoPanel');
+
+
+enum NavigationRouteId {
+    MainPanel,
+    SecondPanel,
+    TodoPanel
+}
+
+const styles = {
+    navCardStyle: RX.Styles.createViewStyle({
+        backgroundColor: '#f5fcff'
+    })
+};
+
+class App extends RX.Component<null, null> {
+    private _navigator: RX.Navigator;
+
+    componentDidMount() {
+        this._navigator.immediatelyResetRouteStack([{
+            routeId: NavigationRouteId.MainPanel,
+            sceneConfigType: RX.Types.NavigatorSceneConfigType.Fade
+        }]);
+    }
+
+    render() {
+        return (
+            <RX.Navigator
+                ref={ this._onNavigatorRef }
+                renderScene={ this._renderScene }
+                cardStyle={ styles.navCardStyle }
+            />
+        );
+    }
+
+    private _onNavigatorRef = (navigator: RX.Navigator) => {
+        this._navigator = navigator;
+    }
+
+    private _renderScene = (navigatorRoute: RX.Types.NavigatorRoute) => {
+        switch (navigatorRoute.routeId) {
+            case NavigationRouteId.MainPanel:
+                return <MainPanel onPressNavigate={ this._onPressNavigate } />
+
+            case NavigationRouteId.SecondPanel:
+                return <SecondPanel onNavigateBack={ this._onPressBack } onShowTodoPanel={ this._onShowTodoPanel }/>
+
+            case NavigationRouteId.TodoPanel:
+                return <EditTodoPanel  onNavigateBack={ this._onPressBack }
+                                onCancelTodo={ this._onPressBack }
+                                onDeleteTodo={ this._onPressBack }
+                                onSaveTodo={ this._onPressBack}
+                  />
+        }
+
+        return null;
+    }
+
+    private _onPressNavigate = () => {
+        this._navigator.push({
+            routeId: NavigationRouteId.SecondPanel,
+            sceneConfigType: RX.Types.NavigatorSceneConfigType.FloatFromRight,
+            customSceneConfig: {
+                hideShadow: true
+            }
+        });
+    }
+
+    private _onShowTodoPanel = () => {
+        this._navigator.push({
+            routeId: NavigationRouteId.TodoPanel,
+            sceneConfigType: RX.Types.NavigatorSceneConfigType.FloatFromRight,
+            customSceneConfig: {
+                hideShadow: true
+            }
+        });
+    }
+
+    private _onPressBack = () => {
+        this._navigator.pop();
+    }
+}
+
+export = App;
