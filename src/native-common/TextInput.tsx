a742224a3fbdb381efb3d7b8a708babb42422a4f
Ramya Senkutuvan <ramyasen@microsoft.com>
2017-07-12 14:42:04 +0200

    #1053906 For textInput we need to seperate setting the focus from screenReaderFocus.

--- src/native-common/TextInput.tsx
+++ src/native-common/TextInput.tsx
@@ -191,7 +191,6 @@ export class TextInput extends RX.TextInput<TextInputState> {
 
     focus() {
         (this.refs['nativeTextInput'] as any).focus();
-        AccessibilityUtil.setAccessibilityFocus(this);
     }
 
     setAccessibilityFocus() {
