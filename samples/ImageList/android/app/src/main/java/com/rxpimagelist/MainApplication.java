bebac3aa7622f946a23770d9123f1ff1be325211
Sergei Dryganets <sergeyd@microsoft.com>
2017-08-28 17:10:09 -0700

    ImageList sample correctly runs on both iOS and Android..

--- samples/ImageList/android/app/src/main/java/com/rxpimagelist/MainApplication.java
+++ samples/ImageList/android/app/src/main/java/com/rxpimagelist/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import org.pgsqlite.SQLitePluginPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,7 +22,6 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new SQLitePluginPackage(),
           new MainReactPackage()
       );
     }
