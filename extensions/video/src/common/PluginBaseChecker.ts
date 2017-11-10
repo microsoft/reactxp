74a4e31f0824b92d775c5ef87c0c349ee6c54f5f
Daniele Pizziconi <danielep@microsoft.com>
2017-11-10 23:36:41 +0100

    adding missing type checkers for macOS

--- extensions/video/src/common/PluginBaseChecker.ts
+++ extensions/video/src/common/PluginBaseChecker.ts
@@ -11,12 +11,14 @@ import Interfaces = require('./Interfaces');
 
 import AndroidPlugin = require('../android/PluginBase');
 import iOSPlugin = require('../ios/PluginBase');
+import macOSPlugin = require('../macos/PluginBase');
 import WebPlugin = require('../web/PluginBase');
 import WindowsPlugin = require('../windows/PluginBase');
 
 /* tslint:disable:no-unused-variable */
 const _typeCheckerAndroid: Interfaces.PluginInterface = AndroidPlugin;
 const _typeCheckeriOS: Interfaces.PluginInterface = iOSPlugin;
+const _typeCheckermacOS: Interfaces.PluginInterface = macOSPlugin;
 const _typeCheckerWeb: Interfaces.PluginInterface = WebPlugin;
 const _typeCheckerWindows: Interfaces.PluginInterface = WindowsPlugin;
 /* tslint:enable:no-unused-variable */
