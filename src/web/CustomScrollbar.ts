39eba55b3e89e0d5d36c16a78678c47d75c3558e
Jose Furia <jofuria@microsoft.com>
2017-11-17 16:38:10 -0800

    We need to verify both vertical and horizontal scroll bars

--- src/web/CustomScrollbar.ts
+++ src/web/CustomScrollbar.ts
@@ -331,20 +331,20 @@ export class Scrollbar {
     private _handleMouseDown(e: React.MouseEvent<any>) {
         const target = e.currentTarget as any;
 
-        if (this._dragging === true || e.target === this._verticalBar.slider || !target) {
+        if (this._dragging === true || !target) {
             this._prevent(e);
             return;
         }
 
         if (this._hasVertical) {
             const eventOffsetY = e.pageY - target.getBoundingClientRect().top;
-            const halfHeight = this._verticalBar.slider.offsetHeight / 2; 
-            const offsetY = (eventOffsetY - this._verticalBar.slider.offsetTop - halfHeight) * this._verticalBar.slider2Scroll;
+            const halfHeight = this._verticalBar.slider!!!.offsetHeight / 2; 
+            const offsetY = (eventOffsetY - this._verticalBar.slider!!!.offsetTop - halfHeight) * this._verticalBar.slider2Scroll!!!;
             this._viewport.scrollTop = offsetY + this._viewport.scrollTop;
         } else if (this._hasHorizontal) {
             const eventOffsetX = e.pageX - target.getBoundingClientRect().left;
-            const halfWidth = this._horizontalBar.slider.offsetWidth / 2; 
-            const offsetX = (eventOffsetX - this._horizontalBar.slider.offsetLeft - halfWidth) * this._verticalBar.slider2Scroll;
+            const halfWidth = this._horizontalBar.slider!!!.offsetWidth / 2; 
+            const offsetX = (eventOffsetX - this._horizontalBar.slider!!!.offsetLeft - halfWidth) * this._verticalBar.slider2Scroll!!!;
             this._viewport.scrollLeft = offsetX + this._viewport.scrollLeft;
         }
     }
