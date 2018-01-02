3ac3754b275b98ea1395809d77b7d66cfb0a73e5
Sergei Dryganets <sergey.dryganets@gmail.com>
2018-01-01 23:43:11 -0800

    Adding option to check if any modal is being displayed. (#398)
    * Adding option to check if any modal is being displayed.
    
    * API updated according to proposal in review. Documentation updated

--- src/native-common/Modal.ts
+++ src/native-common/Modal.ts
@@ -15,8 +15,8 @@ import RX = require('../common/Interfaces');
 import Types= require('../common/Types');
 
 export class Modal extends RX.Modal {
-    isDisplayed(modalId: string): boolean {
-        if (!modalId || modalId === '') {
+    isDisplayed(modalId?: string): boolean {
+        if (modalId === '') {
             throw new Error(`modalId must be a non-empty string. Actual: ${modalId}`);
         }
 
