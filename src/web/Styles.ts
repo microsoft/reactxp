d18ad394c6affc00372f52be7f5c1b963d0d3225
Brent Erickson <berickson1@users.noreply.github.com>
2018-02-13 17:06:23 -0800

    Revert "Fixed bug in web animation that caused certain CSS properties not to animate correctly because transition was specifying the attribute using camel case rather than CSS (hyphenated) case. This affected attributes like "backgroundColor"."
    This reverts commit 9bb3785d35dcc2f916ccb5a4973064f0ddd6ae47.

--- src/web/Styles.ts
+++ src/web/Styles.ts
@@ -201,7 +201,7 @@ export class Styles extends RX.Styles {
     });
 
     // Converts a property from JavaScript style (camel-case) to CSS style (lowercase with hyphens).
-    convertJsToCssStyle(prop: string): string {
+    private _convertJsToCssStyle(prop: string): string {
         let cssString = '';
 
         if (prop) {
@@ -216,6 +216,22 @@ export class Styles extends RX.Styles {
         }
 
         return cssString;
+    }
+
+    _cssPropertyAliasesCssStyle = memoize(() => {
+        let jsStyleAliases = this._getCssPropertyAliasesJsStyle();
+
+        let aliases: CssAliasMap = {};
+
+        _.each(_.keys(jsStyleAliases), prop => {
+            aliases[prop] = this._convertJsToCssStyle(jsStyleAliases[prop]);
+        });
+
+        return aliases;
+    });
+
+    getCssPropertyAliasesCssStyle(): {[key: string]: string} {
+        return this._cssPropertyAliasesCssStyle();
     }
 
     getParentComponentName(component: any): string {
