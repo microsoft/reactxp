b51c6d101f652c39e72210f1cdae5dee66fb5a72
Shane Fitzpatrick <fitzpasd@gmail.com>
2018-02-22 19:05:22 -0800

    Platform select() method - make it easier to get platform specific values (#520)

--- src/native-common/Platform.ts
+++ src/native-common/Platform.ts
@@ -16,6 +16,11 @@ export class Platform extends RX.Platform {
     getType(): Types.PlatformType {
         return RN.Platform.OS;
     }
+
+    select<T>(specifics: { [ platform in Types.PlatformType | 'default' ]?: T }): T | undefined {
+        const platformType = this.getType();
+        return platformType in specifics ? specifics[platformType] : specifics.default;
+    }
 }
 
 export default new Platform();
