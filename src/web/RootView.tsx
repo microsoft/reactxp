5284b348d1d63e8f64315dd419d6d06073e76e50
Ramya Senkutuvan <ramyasen@microsoft.com>
2018-03-16 06:11:35 +0100

    IN mac the first text that gets added to live regions is skipped when root view is created. Making sure additions are counted as well (#550)

--- src/web/RootView.tsx
+++ src/web/RootView.tsx
@@ -336,7 +336,7 @@ export class RootView extends React.Component<RootViewProps, RootViewState> {
                     style={ _styles.liveRegionContainer as any }
                     aria-live={ AccessibilityUtil.accessibilityLiveRegionToString(Types.AccessibilityLiveRegion.Polite) }
                     aria-atomic={ 'true' }
-                    aria-relevant={ 'text' }
+                    aria-relevant={ 'additions text' }
                 >
                     { announcement }
                 </div>
