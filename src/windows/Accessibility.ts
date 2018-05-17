f23237cca18ad71fc8ebb219237d154112a838f6
Yury Zholobov <yuzholob@microsoft.com>
2018-05-17 15:12:42 -0700

    fix UWP accessibility announce temporary same way Android code path hacked/fixed it (for consistency)

--- src/windows/Accessibility.ts
+++ src/windows/Accessibility.ts
@@ -6,7 +6,7 @@
 */
 
 import RN = require('react-native');
-import { Accessibility as NativeAccessibility } from '../native-common/Accessibility';
+import { Accessibility as NativeAccessibility, default as parentInstance } from '../native-common/Accessibility';
 
 export class Accessibility extends NativeAccessibility {
     private _isHighContrast = RN.AccessibilityInfo.initialHighContrast || false;
@@ -29,6 +29,19 @@ export class Accessibility extends NativeAccessibility {
     isHighContrastEnabled(): boolean {
         return this._isHighContrast;
     }
+
+    announceForAccessibility(announcement: string): void {
+        // We cannot just call super.announceForAccessibility here, because RootView subscribes on this
+        // parent class singleton instance. Calling Accessibility.announceForAccessibility from the consumer app
+        // will then create a different event and the announcements won't work. Instead, we just call the
+        // instance method directly.
+        //
+        // This dirty hack was copied from android/Accessibility.ts but it's temporary.
+        // TODO: Decide on which pattern to use for "extending"/"inheriting" classes while not
+        // having problem with duplicate state like here. And then replace this dirty hack (and all the other 
+        // bugs in existing code - this class and others) with proper pattern.
+        parentInstance.announceForAccessibility(announcement);
+    }    
 }
 
 export default new Accessibility();
