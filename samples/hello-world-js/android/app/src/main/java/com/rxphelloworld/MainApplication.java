396d12b90ed70ea6b776b2de578f6cb63284510d
michaelperel <mperel@princeton.edu>
2018-05-18 18:56:16 -0400

    Fixed import from RNSvgPackage to SvgPackage (#635)

--- samples/hello-world-js/android/app/src/main/java/com/rxphelloworld/MainApplication.java
+++ samples/hello-world-js/android/app/src/main/java/com/rxphelloworld/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.brentvatne.react.ReactVideoPackage;
-import com.horcrux.svg.RNSvgPackage;
+import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new ReactVideoPackage(),
-            new RNSvgPackage()
+            new SvgPackage()
       );
     }
   };
