6207f6440a4274d4bf9e13364e78e6e57c6355bd
Simone Locci <simone.locci@cloudesire.com>
2018-06-14 23:58:58 +0200

    Remove custom react-native.d.ts (#696)

--- extensions/video/src/windows/Video.tsx
+++ extensions/video/src/windows/Video.tsx
@@ -7,18 +7,13 @@
 * Windows-specific implementation of the cross-platform Video abstraction.
 */
 
+import extend = require('lodash/extend');
 import React = require('react');
 import RN = require('react-native');
 import RX = require('reactxp');
 
 import Interfaces = require('../common/Interfaces');
 import Types = require('../common/Types');
-
-var _styles = RN.StyleSheet.create({
-    bg: {
-        backgroundColor: 'red'
-    }
-});
 
 export interface VideoState {
     isPlaying?: boolean;
@@ -28,9 +23,13 @@ export interface VideoState {
 class Video extends RX.Component<Types.VideoProps, VideoState> {
     // TODO(uwp): #694149 Not implemented
     render() {
+        let combinedStyles = extend(RX.Styles.combine(this.props.style), {
+            backgroundColor: 'red'
+        });
+
         return (
             <RN.View
-                style={ [this.props.style, _styles.bg] }
+                style={ combinedStyles }
             >
                 <RN.Text>Video</RN.Text>
             </RN.View>
