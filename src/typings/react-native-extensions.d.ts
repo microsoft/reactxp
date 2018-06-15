891bcead9f09d0ea4d766f9898f404925ce8e99b
Alexander T <alexander.v.tarasyuk@gmail.com>
2018-06-15 06:49:08 +0300

    Remove event type casting (#698)

--- src/typings/react-native-extensions.d.ts
+++ src/typings/react-native-extensions.d.ts
@@ -83,8 +83,4 @@ declare module 'react-native' {
     interface ExtendedAccessibilityInfoStatic extends RN.AccessibilityInfoStatic {
         static initialHighContrast: boolean|undefined;
     }
-
-    interface ExtendedNativeSyntheticEvent extends NativeSyntheticEvent {
-        persist?: () => void;
-    }
 }
