ab77fb20c79eec2b5a225ed503b37c02c4756246
Alexander T <alexander.v.tarasyuk@gmail.com>
2018-06-28 09:05:50 +0300

    Change WebView types (#729)

--- src/native-common/WebView.tsx
+++ src/native-common/WebView.tsx
@@ -26,30 +26,24 @@ export class WebView extends React.Component<Types.WebViewProps, Types.Stateless
     private _mountedComponent: any;
 
     render() {
-        let styles = [_styles.webViewDefault, this.props.style];
-        let source: any = this.props.source;
-        if (this.props.url) {
-            source = {
-                uri: this.props.url,
-                headers: this.props.headers
-            };
-        }
+        const styles = [_styles.webViewDefault, this.props.style] as RN.StyleProp<RN.ViewStyle>;
+        const source = this._buildSource();
 
         return (
             <RN.WebView
                 ref={ this._onMount }
-                style={ styles as RN.StyleProp<RN.ViewStyle> }
-                onNavigationStateChange={ this.props.onNavigationStateChange }
-                onShouldStartLoadWithRequest={ this.props.onShouldStartLoadWithRequest }
+                style={ styles }
                 source={ source }
-                onLoad={ this.props.onLoad }
                 startInLoadingState={ this.props.startInLoadingState }
                 javaScriptEnabled={ this.props.javaScriptEnabled }
                 injectedJavaScript={ this.props.injectedJavaScript }
                 domStorageEnabled={ this.props.domStorageEnabled }
                 scalesPageToFit={ this.props.scalesPageToFit }
-                onError={ this.props.onError }
+                onNavigationStateChange={ this.props.onNavigationStateChange }
+                onShouldStartLoadWithRequest={ this.props.onShouldStartLoadWithRequest }
                 onLoadStart={ this.props.onLoadStart }
+                onLoad={ this.props.onLoad }
+                onError={ this.props.onError }
                 onMessage={ this.props.onMessage ? this._onMessage : undefined }
                 testID={ this.props.testId }
             />
@@ -60,13 +54,13 @@ export class WebView extends React.Component<Types.WebViewProps, Types.Stateless
         this._mountedComponent = component;
     }
 
-    protected _onMessage = (e: RN.NativeSyntheticEvent<any>) => {
+    protected _onMessage = (e: RN.NativeSyntheticEvent<RN.WebViewMessageEventData>) => {
         if (this.props.onMessage) {
             // Clone the original event because RN reuses events.
             let event: RX.Types.WebViewMessageEvent = _.clone(e) as any;
 
             // Add the data element.
-            event.data = (e.nativeEvent as any).data;
+            event.data = e.nativeEvent.data;
             event.origin = '*';
 
             event.stopPropagation = () => {
@@ -83,6 +77,20 @@ export class WebView extends React.Component<Types.WebViewProps, Types.Stateless
 
             this.props.onMessage(event);
         }
+    }
+
+    private _buildSource(): RN.WebViewUriSource | RN.WebViewHtmlSource | undefined {
+        const { headers, source, url } = this.props;
+
+        if (url) {
+            return { headers, uri: url };
+        }
+
+        if (source) {
+            return source;
+        }
+
+        return undefined;
     }
 
     postMessage(message: string, targetOrigin: string = '*') {
