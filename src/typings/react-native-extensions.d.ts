e241593f571b6502e3ab33ec96a0f68eaefced0e
Radu Simionescu <radus@microsoft.com>
2018-10-08 19:39:14 -0700

    RTL changed event

--- src/typings/react-native-extensions.d.ts
+++ src/typings/react-native-extensions.d.ts
@@ -39,6 +39,10 @@ declare module 'react-native' {
         useWebKit?: boolean;
     }
 
+    interface RtlEventNativePayload {
+        isRTL:  boolean;
+    }
+
     abstract class ReactNativeBaseComponent<P, S> extends React.Component<P, S> {
         setNativeProps(nativeProps: P): void;
         focus(): void;
