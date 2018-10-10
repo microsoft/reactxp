327150b4b07220a3c3d039031bbc2bff6193f886
Radu Simionescu <reseul@hotmail.com>
2018-10-10 07:20:18 -0700

    RTL changed event (#848)

--- src/native-common/International.ts
+++ src/native-common/International.ts
@@ -12,6 +12,16 @@ import * as RN from 'react-native';
 import * as RX from '../common/Interfaces';
 
 export class International implements RX.International {
+    private _isRTL: boolean;
+
+    constructor() {
+        // RN.I18nManager.isRTL is a constant, a good start.
+        this._isRTL = RN.I18nManager.isRTL;
+
+        // Register for changes (some platforms may never raise this event)
+        RN.DeviceEventEmitter.addListener('isRTLChanged', (payload: RN.RtlEventNativePayload) => {this._isRTL = payload.isRTL; });
+    }
+
     allowRTL(allow: boolean): void {
         RN.I18nManager.allowRTL(allow);
     }
@@ -21,7 +31,7 @@ export class International implements RX.International {
     }
 
     isRTL(): boolean {
-        return RN.I18nManager.isRTL;
+        return this._isRTL;
     }
 }
 
