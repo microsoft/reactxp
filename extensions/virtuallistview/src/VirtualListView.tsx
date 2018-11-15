d5b33f1c6cae8549f924ac67fb5cdb72a16482cc
Brent Erickson <berickson1@users.noreply.github.com>
2018-11-14 16:49:10 -0800

    Handle keycodes for RN in VLV Caused by #419

--- extensions/virtuallistview/src/VirtualListView.tsx
+++ extensions/virtuallistview/src/VirtualListView.tsx
@@ -140,6 +140,7 @@ const _styles = {
 const _isNativeAndroid = RX.Platform.getType() === 'android';
 const _isNativeIOS = RX.Platform.getType() === 'ios';
 const _isNativeMacOs = RX.Platform.getType() === 'macos';
+const _isWeb = RX.Platform.getType() === 'web';
 
 // How many items with unknown heights will we allow? A larger value will fill the view more
 // quickly but will result in a bunch of long-running work that can cause frame skips during
@@ -158,9 +159,10 @@ const _scrollViewRef = 'scrollview';
 const _virtualKeyPrefix = 'vc_';
 const _accessibilityVirtualKeyPrefix = 'ac_';
 
-// Key codes used on web
-const _keyCodeUpArrow = 38;
-const _keyCodeDownArrow = 40;
+// Key codes used on web/RN (keycodes for arrows are different between web and native, unfortunately)
+// (a resolution for https://github.com/Microsoft/reactxp/issues/419 will make this look better, hopefuly)
+const _keyCodeUpArrow = _isWeb ? 38 : 19;
+const _keyCodeDownArrow = _isWeb ? 40 : 20;
 
 // tslint:disable:override-calls-super
 export class VirtualListView<ItemInfo extends VirtualListViewItemInfo> 
